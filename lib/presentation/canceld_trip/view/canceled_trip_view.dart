a7f367bd66bbb5bc792d0fbac15f0c31a6ccd697
Ehab Roushdy <Yasserhassan@1>
2023-03-20 20:44:36 +0200

    adding canceled trip immediate trip scheduled trip files (view - viewmodel)

---
+++ lib/presentation/canceld_trip/view/canceled_trip_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScheduledTripView extends StatefulWidget {
+  const ScheduledTripView({Key? key}) : super(key: key);
+
+  @override
+  State<ScheduledTripView> createState() => _ScheduledTripViewState();
+}
+
+class _ScheduledTripViewState extends State<ScheduledTripView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
