60b90f7e52ecc89558ac7d1444b7316de0c09d7a
Yasserhassan <yasserhassan.it.95@gmail.com>
2024-11-01 19:31:36 +0200

    return verify otp view

--- lib/presentation/otp/view/verify_otp_view.dart
+++ lib/presentation/otp/view/verify_otp_view.dart
@@ -57,19 +57,19 @@ class _VerifyOtpViewState extends State<VerifyOtpView> {
   @override
   void initState() {
     //TODO Return sendOtp Feature
-    // BlocProvider.of<VerifyOtpBloc>(context)
-    //     .add(SendOtpEvent(widget.mobileNumberForApi));
-    if (widget.registerAs == RegistrationConstants.captain) {
-      BlocProvider.of<LoginBloc>(context).add(MakeLoginEvent(
-          /*'1234567890'*/
-          widget.mobileNumberForApi,
-          widget.countryCode));
-    } else {
-      BlocProvider.of<LoginBloc>(context).add(MakeLoginBOEvent(
-          /*'1234567890'*/
-          widget.mobileNumberForApi,
-          widget.countryCode));
-    }
+    BlocProvider.of<VerifyOtpBloc>(context)
+        .add(SendOtpEvent(widget.mobileNumberForApi));
+    // if (widget.registerAs == RegistrationConstants.captain) {
+    //   BlocProvider.of<LoginBloc>(context).add(MakeLoginEvent(
+    //       /*'1234567890'*/
+    //       widget.mobileNumberForApi,
+    //       widget.countryCode));
+    // } else {
+    //   BlocProvider.of<LoginBloc>(context).add(MakeLoginBOEvent(
+    //       /*'1234567890'*/
+    //       widget.mobileNumberForApi,
+    //       widget.countryCode));
+    // }
     super.initState();
   }
 
